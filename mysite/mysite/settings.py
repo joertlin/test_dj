1ed8372cdc8032d91b8c69a17aa5f62a327005b7
Jeremiah Oertling <Jeremiah.Oertling@gmail.com>
2015-05-05 22:53:59 -0500

    trying to set up a postgres db.  created a db and user, those details are now in the settings.py file.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_django_test',
+        'USER': 'joe',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
