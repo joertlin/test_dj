10e7a435b34c65921e499636f54ec93cf52840aa
Jeremiah Oertling <Jeremiah.Oertling@gmail.com>
2015-05-09 21:56:57 -0500

    Continuing the django tutorial.  the admin.py file is getting updated to the choice and question functionality wehre the admin has control over the contents.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Question
 # Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None,		{fields 'pub_date','question_text']
+
+admin.site.register(Question, QuestionAdmin)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
 class Question(models.Model):
@@ -9,7 +11,7 @@ class Question(models.Model):
 		return self.question_text
 
 	def was_published_recently(self):
-		return self.pub_date >= timezon.now() - datetime.timedelta(days = 1)
+		return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question)
